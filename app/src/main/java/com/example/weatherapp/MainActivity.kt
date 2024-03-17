17238556d6d05b16bbd055852c609344b4cad120
Yoann <yoann.chamillard@gmail.com>
2024-03-17 15:48:53 +0100

    220.Prepare the project - Enable location

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.example.weatherapp
 
 import android.content.Context
+import android.content.Intent
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isLocationEnabled()) {
+
+            Toast.makeText(this@MainActivity, "The location is not enabled", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this@MainActivity, "The location is already turned ON", Toast.LENGTH_SHORT).show()
+        }
     }
 
+    private fun isLocationEnabled() : Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
 }
